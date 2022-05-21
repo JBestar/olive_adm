f84c6a95c7741af86b39b8be61a60c706f9e758f
Bestar <bestar1105@hotmail.com>
2022-05-21 15:55:55 +0900

    20220521

--- app/Models/CsBet_model.php
+++ app/Models/CsBet_model.php
@@ -114,7 +114,7 @@ class CsBet_model extends Model
             $strWhere.=" bet_game_type = '".$arrReqData['mode']."' ";
         }
         $nStartRow = ($arrReqData['page']-1) * $arrReqData['count'] ;
-        $strWhere.=" ORDER BY bet_fid DESC LIMIT ".$nStartRow.", ".$arrReqData['count'];
+        $strWhere.=" ORDER BY bet_time DESC LIMIT ".$nStartRow.", ".$arrReqData['count'];
         
         $strSql = "";
         $strSql .= "SELECT bet_fid, bet_idx, bet_mb_uid, bet_round_no, bet_time, bet_money, bet_win_money, bet_player_id, bet_game_type, bet_table_code, ";
@@ -154,7 +154,7 @@ class CsBet_model extends Model
             
         }
         $strSql .= " LEFT JOIN ".$this->mGameTable." ON ".$tbBetSearch.".bet_table_code = ".$this->mGameTable.".tid ";
-        $strSql .= " ORDER BY bet_fid DESC";
+        $strSql .= " ORDER BY bet_time DESC";
 
         $query = $this -> db -> query($strSql);
         $result = $query -> getResult();
