3a7293318d8d1a533c5cac8575e3da96bcad177b
jbestar <jbestar1105@gmail.com>
2024-01-11 23:45:08 +0900

    20240111-future

--- app/Models/Clean_model.php
+++ app/Models/Clean_model.php
@@ -30,7 +30,7 @@ class Clean_Model extends Model {
         $strSql = " DELETE FROM bet_reward_mn_st WHERE rw_end < '".$strDate."' ";
         $this -> db -> query($strSql);
 
-        $strSql = " DELETE FROM bet_powerball WHERE bet_time < '".$strDate."' ";
+        $strSql = " DELETE FROM bet_pball WHERE bet_time < '".$strDate."' ";
         $this -> db -> query($strSql);
         
         $strSql = " DELETE FROM bet_ebal WHERE bet_time < '".$strDate."' ";
@@ -51,7 +51,7 @@ class Clean_Model extends Model {
         $this->db->query("TRUNCATE bet_slot");
         $this->db->query("TRUNCATE bet_casino");
         $this->db->query("TRUNCATE bet_balance");
-        $this->db->query("TRUNCATE bet_powerball");
+        $this->db->query("TRUNCATE bet_pball");
         $this->db->query("TRUNCATE bet_holdem");
         $this->db->query("TRUNCATE bet_reward");
         $this->db->query("TRUNCATE bet_reward_st");
@@ -59,7 +59,6 @@ class Clean_Model extends Model {
         $this->db->query("TRUNCATE bet_reward_mn_st");
         $this->db->query("TRUNCATE bet_follow");
         $this->db->query("TRUNCATE block_list");
-        $this->db->query("TRUNCATE log_modify");
         $this->db->query("TRUNCATE member_charge");
         $this->db->query("TRUNCATE member_exchange");
         $this->db->query("TRUNCATE money_history");
@@ -67,8 +66,11 @@ class Clean_Model extends Model {
         $this->db->query("TRUNCATE money_transfer");
         $this->db->query("TRUNCATE sessions");
         $this->db->query("TRUNCATE sess");
+
+        $this->db->query("TRUNCATE log_modify");
         $this->db->query("TRUNCATE sess_log");
         $this->db->query("TRUNCATE sess_try");
+        $this->db->query("TRUNCATE captcha");
         $this->db->query("TRUNCATE bet_ebal");
         $this->db->query("TRUNCATE bet_ebal_st");
         $this->db->query("TRUNCATE bet_balance");
@@ -76,7 +78,7 @@ class Clean_Model extends Model {
         $tmNow = time();
         $strDate = date('Y-m-d', strtotime("-2 months", $tmNow));
 
-        $strSql = " DELETE FROM round_powerball WHERE round_date < '".$strDate."' ";
+        $strSql = " DELETE FROM round_pball WHERE round_time < '".$strDate."' ";
         $this -> db -> query($strSql);
 
         $strSql = " DELETE FROM board_notice WHERE notice_type != '".NOTICE_BOARD."' ";
@@ -84,6 +86,7 @@ class Clean_Model extends Model {
 
         $strSql = " DELETE FROM member WHERE mb_level < '".LEVEL_ADMIN."' ";
         $this -> db -> query($strSql);
+        $this->db->query("TRUNCATE member_conf");
 
         return 1;
     }
@@ -101,10 +104,20 @@ class Clean_Model extends Model {
             $this->db->query("TRUNCATE bet_reward_mn_st");
             $this->db->query("TRUNCATE money_history");
             $this->db->query("TRUNCATE money_history_st");
+            $this->db->query("TRUNCATE money_transfer");
             $this->db->query("TRUNCATE bet_casino");
             $this->db->query("TRUNCATE bet_slot");
             $this->db->query("TRUNCATE bet_holdem");
-            $this->db->query("TRUNCATE bet_powerball");
+            $this->db->query("TRUNCATE bet_pball");
+            $this->db->query("TRUNCATE round_pball");
+
+            $this->db->query("TRUNCATE log_modify");
+            $this->db->query("TRUNCATE sess_log");
+            $this->db->query("TRUNCATE sess_try");
+            $this->db->query("TRUNCATE captcha");
+
+            $strSql = " DELETE FROM board_notice WHERE notice_type != '".NOTICE_BOARD."' ";
+            $this -> db -> query($strSql);
         } else if(strlen($date) > 0) {
 
             $partName = "P_".str_replace("-", "", $date)."_000000";
@@ -134,7 +147,10 @@ class Clean_Model extends Model {
             $table = "bet_holdem";
             $this->dropPartitions($table, $partName);
 
-            $table = "bet_powerball";
+            $table = "bet_pball";
+            $this->dropPartitions($table, $partName);
+
+            $table = "round_pball";
             $this->dropPartitions($table, $partName);
 
             $strSql = " DELETE FROM bet_reward_st WHERE rw_end < '".$date."' ";
@@ -149,10 +165,26 @@ class Clean_Model extends Model {
             $strSql = " DELETE FROM money_history_st WHERE money_end < '".$date."' ";
             $this -> db -> query($strSql);
 
-            writeLog("clean Partition END");
+            $strSql = " DELETE FROM money_transfer WHERE trans_time < '".$date."' ";
+            $this -> db -> query($strSql);
+
+            $strSql = " DELETE FROM board_notice WHERE notice_type != '".NOTICE_BOARD."' AND notice_time_create < '".$date."' ";
+            $this -> db -> query($strSql);
+
+            $strSql = " DELETE FROM log_modify WHERE log_time < '".$date."' ";
+            $this -> db -> query($strSql);
+            
+            $strSql = " DELETE FROM sess_log WHERE log_time < '".$date."' ";
+            $this -> db -> query($strSql);
+
+            $strSql = " DELETE FROM sess_try WHERE log_time < '".$date."' ";
+            $this -> db -> query($strSql);
+            
+            $strSql = " DELETE FROM captcha WHERE cap_time < '".$date."' ";
+            $this -> db -> query($strSql);
 
         }
-
+        writeLog("clean Partition END");
 
         return 1;
     }
